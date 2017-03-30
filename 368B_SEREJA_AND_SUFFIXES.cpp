8bd2d8b7a55794818539f974fe1794319df6f887
saurabh kumar <itsmeonly06@gmail.com>
2017-03-30 08:44:05 +0530

    Add files via upload

--- 368B_SEREJA_AND_SUFFIXES.cpp
+++ 368B_SEREJA_AND_SUFFIXES.cpp
@@ -1,43 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool D[1000001];
 int main()
 {
     int n,m;
     cin>>n>>m;
-    int A[n];
-    int R[n];
-    for(int i=0;i<n;i++)
+    int A[n+1];
+    int R[n+1];
+    for(int i=1;i<=n;i++)
     {
        cin>>A[i];
-       R[i]=n-i;
-
     }
-    R[n-1]=1;
-
-    for(int i=n-2;i>=0;i--)
+    int count=0;
+    for(int i=n;i>0;i--)
     {
-        int flag=0;
-        for(int j=n-1;j>i;j--)
-            {
-
-                if(A[i]==A[j]&&R[i]<=n-i)
-                {
-                    flag=1;
-                    //cout<<"   ------"<<A[i]<<endl;
-                    R[i]=R[i+1];
-                }
-
-            }
-            if(flag==0)
-                R[i]=R[i+1]+1;
+        if(D[A[i]]==0)
+        {
+            count++;
+            R[i]=count;
+            D[A[i]]=1;
+        }
+        else
+            R[i]=count;
 
     }
-    for(int i=0;i<m;i++)
-        {
-            int x;
-            cin>>x;
-            cout<<R[x-1]<<endl;
-        }
+    //for(int i=1;i<=n;i++)
+        //cout<<R[i]<<endl;
+        for(int i=0;i<m;i++)
+    {
+        int x;
+        cin>>x;
+        cout<<R[x]<<"\n";
+
+
+    }
+
+
 
 
 
