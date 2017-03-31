db2347dc85cda854b1659b7751ae73c912754e3a
saurabh kumar <itsmeonly06@gmail.com>
2017-03-31 13:15:26 +0530

    Add files via upload

--- 171A. Mysterious numbers - 1.cpp
+++ 171A. Mysterious numbers - 1.cpp
@@ -1,79 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-//chutiyapa code -_-
-int A[9],B[9],R[10];
 int main()
 {
-   int a,b;
-   cin>>a>>b;
-   int ac=9,bc=9;
-  int a1=a,b1=b;
-  while(a1>0)
-  {
-    A[ac]=a1%10;
-    a1=a1/10;
-    ac--;
- }
- while(b1>0)
-  {
-    B[bc]=b1%10;
-    b1=b1/10;
-    bc--;
- }
- ac++;bc++;
+  int n,m;
+  cin>>n>>m;
+   int t=0;
+   int count1=0,count2=0;
 
- if(a==0&&b==0)
-    cout<<0<<endl;
- else if(a>b)
- {
-     int i=ac,j=9;
-    while(i<=9&&j>=ac)
-    {
-        R[i]=A[i]+B[j]+R[i];
-        if(R[i]>9)
-        {
-           R[i]=R[i] %10;
-           R[i+1]=R[i]/10;
-        }
-        i++;
-        j--;
-
+   while(m>0)
+   {
+       t=m%10+t*10;
+       m=m/10;
+       count1++;
     }
-}
-else
-{
-    int i=bc,j=9;
 
-    while
-        (i<=9&&j>=bc)
-        {
-            R[i]=A[i]+B[j]+R[i];
-            if(R[i]>9)
-        {
-           R[i]=R[i] %10;
-           R[i+1]=R[i]/10;
-        }
-        i++;
-        j--;
+    cout<<t+n<<endl;
 
-        }
-}
-int flag=0;
-for(int i=0;i<10;i++)
-{
-    if(R[i]==0&&flag==0)
-    {
-        flag=0;
-    }
-    else
-    {
-      flag=1;
-    cout<<R[i];
-    }
-}
-cout<<endl;
+
+
 
 
     return 0;
-
 }
